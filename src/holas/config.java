8af543a96f317e024f371b2d440185f7af42958f
Ulises Rosales <urosales507@gmail.com>
2022-08-13 20:15:38 -0400

    prueba git

---
+++ src/holas/config.java
@@ -0,0 +1,9 @@
+package holas;
+
+public class config {
+	public static double suma(int num1, double num2){
+		double num3=num1+num2;
+		System.out.println(num3);
+		return num3;
+	}
+}
---
+++ src/holas/index.java
@@ -0,0 +1,15 @@
+package holas;
+
+public class index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Wuena hora");
+		int num1 = 1;
+		double num2= 1.2;
+		config.suma(num1, num2);
+	}
+
+}
+
+
